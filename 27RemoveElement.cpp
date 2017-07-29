3eb78e8f52bded9572eaa46457b2c4eeda517b0b
kaka2634 <996529090@qq.com>
2017-07-29 23:41:54 +0800

    add 167

--- 27RemoveElement.cpp
+++ 27RemoveElement.cpp
@@ -1,21 +1,21 @@
 /*
-* 27.Remove Element
-* Author: Kun
-* Date:2017-07-29
-* Solution: Use two variable i,j as index for nums. i record after-removed index, j record compare index.
-*/
+ * 27. Remove Element
+ * Author: Kun
+ * Date:2017-07-29
+ * Solution: Use two variable i,j as index for nums. i record after-removed index, j record compare index.
+ */
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
+int removeElement(vector<int>& nums, int val) {
         if(nums.size()==0) return 0;
         int i=0;
         int j=0;
         while(j<nums.size())
         {
-            if(nums[j]!=val)
-                nums[i++]=nums[j];
-            j++;
+                if(nums[j]!=val)
+                        nums[i++]=nums[j];
+                j++;
         }
         return i;
-    }
+}
 };
